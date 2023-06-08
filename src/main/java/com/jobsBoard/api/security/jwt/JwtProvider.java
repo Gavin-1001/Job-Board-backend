4f9332daaf75a35806f64e84ce6105ac8575d8d7
Gavin Shelley <gavinshelley10@gmail.com>
2023-06-08 14:40:54 +0100

    Added JwtProvderImpl methods

--- src/main/java/com/jobsBoard/api/security/jwt/JwtProvider.java
+++ src/main/java/com/jobsBoard/api/security/jwt/JwtProvider.java
@@ -1,4 +1,15 @@
 package com.jobsBoard.api.security.jwt;
 
+import com.jobsBoard.api.security.UserPrinciple;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
 public interface JwtProvider {
+    String generateToken(UserPrinciple auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    Claims extractClaims(HttpServletRequest request);
 }
